8d09305f076df87da00c710520fd27c7743cc65b
Mindcool25 <mindcool24@jsociety.xyz>
2022-03-05 16:00:41 -0700

    Added unit tests, all currently pass

---
+++ tests.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import game
+
+
+matrix = [
+    [0, 0, 0,  0, 0, 0],
+    [0, 0, 0,  0, 0, 0],
+    [0, 0, 0,  0, 0, 0],
+
+    [0, 0, 0,  0, 0, 0],
+    [0, 0, 0,  0, 0, 0],
+    [0, 0, 0,  0, 0, 0]
+    ]
+
+def testPlace():
+    p = game.pentago()
+    goal = [[0, 1, 0,  0, 0, 0],
+            [0, 0, 0,  0, 1, 0],
+            [0, 0, 0,  0, 0, 0],
+
+            [1, 0, 0,  0, 0, 0],
+            [0, 0, 0,  0, 0, 0],
+            [0, 0, 0,  0, 0, 1]]
+    p.place("a2")
+    p.place("b5")
+    p.place("c1")
+    p.place("d9")
+    if goal == p.matrix:
+        del p
+        return "Passed"
+    else:
+        del p
+        return "Failed"
+
+def testRotation():
+    r = game.pentago()
+    goal = [[0, 0, 1,  0, 0, 1],
+            [0, 0, 0,  0, 0, 0],
+            [0, 0, 0,  0, 0, 0],
+
+            [0, 0, 1,  0, 0, 1],
+            [0, 0, 0,  0, 0, 0],
+            [0, 0, 0,  0, 0, 0]]
+    r.place("a1")
+    r.place("b1")
+    r.place("c1")
+    r.place("d1")
+    r.rotate("1")
+    r.rotate("2")
+    r.rotate("3")
+    r.rotate("4")
+    if goal == r.matrix:
+        del r
+        return "Passed"
+    else:
+        print(r.matrix)
+        r.printBoard()
+        del r
+        return "Failed"
+
+
+def main():
+    print("Testing Placements: " + testPlace())
+    print("Testing Rotations: " + testRotation())
+    return
+
+if __name__ == "__main__":
+    main()
